6772c6d11bcd1287eed5411669ccc39d024c2ae4
Esron Dtamar <esron.dtamar@gmail.com>
2019-04-06 20:36:53 -0300

    using mongodb as database

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+
+connectUri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+mongoose.connect(
+  connectUri,
+  {useNewUrlParser: true}
+)
 
 app.use(logger('dev'));
 app.use(express.json());

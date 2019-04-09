10c51dc3d3ccc97c1b26c58d96942833cff45b43
Esron Dtamar <esron.dtamar@gmail.com>
2019-04-08 23:35:06 -0300

    uses the UserController on the user router

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/UserController');
+
+/* Creates a new user */
+router.post(
+  '/',
+  userController.validate('createUser'),
+  userController.createUser
+);
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get(
+  '/',
+  userController.listUsers
+);
 
 module.exports = router;

0c31dadf82f94c8c8fe3e194e71d57e00c50db26
Esron Dtamar <esron.dtamar@gmail.com>
2019-04-09 23:58:53 -0300

    implements get user and update user methods

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,4 @@
-const { validationResult, checkSchema } = require('express-validator/check');
+const { param, validationResult, checkSchema } = require('express-validator/check');
 
 const User = require('../models/User');
 
@@ -12,6 +12,10 @@ UserController.validate = (method) => {
           in: ['body'],
           exists: {
             errorMessage: 'Name field is required',
+          },
+          isEmpty: {
+            errorMessage: 'Name field cannot be empty',
+            negated: true,
           },
           trim: true,
           escape: true,
@@ -33,36 +37,154 @@ UserController.validate = (method) => {
                 }
               });
             }
-          }
-        }
+          },
+        },
+      });
+    }
+    case 'getUser': {
+      return [
+        param('userId', 'Invalid userId')
+          .isMongoId(),
+      ];
+    }
+    case 'updateUser': {
+      return checkSchema({
+        userId: {
+          in: ['params'],
+          isMongoId: {
+            errorMessage: 'Invalid userId',
+          },
+        },
+        name: {
+          in: ['body'],
+          optional: true,
+          trim: true,
+          escape: true,
+          stripLow: true,
+          isEmpty: {
+            errorMessage: 'Name field cannot be empty',
+            negated: true,
+          },
+        },
+        email: {
+          in: ['body'],
+          optional: true,
+          isEmail: {
+            errorMessage: 'Invalid Email',
+          },
+          custom: {
+            options: (value) => {
+              return User.find({email: value}).then(user => {
+                if (user.length) {
+                  return Promise.reject('Email already in use');
+                }
+              });
+            },
+          },
+        },
+        status: {
+          in: ['body'],
+          optional: true,
+          isIn: {
+            options: ['enabled', 'disabled'],
+            errorMessage: `Status must be one of the following ['enabled', 'disabled']`,
+          },
+          isEmpty: {
+            errorMessage: 'Name field cannot be empty',
+            negated: true,
+          },
+        },
       });
     }
   }
 }
 
 UserController.createUser = (req, res) => {
-  const errors = validationResult(req) // To get the result of above validade fn
-  if (!errors.isEmpty()){
-    return res.status(422).send({errors: errors.array()});
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    return res.status(422).send({ errors: errors.array() })
   }
 
   const { name, email } = req.body;
 
   User.create({ name, email, status: 'enabled' })
-    .then(user => res.status(200).send(user))
+    .then(user => res.status(200).send({ data: user }))
     .catch(err => res.status(500).send({
-      error: err,
+      errors: err,
       message: 'There was a problem creating the user.',
     }));
 }
 
 UserController.listUsers = (_, res) => {
   User.find({}, { password: 0 })
-    .then((users) => res.status(200).send(users))
+    .then((users) => res.status(200).send({ data: users }))
     .catch(err => res.status(500).send({
-      error: err,
+      errors: err,
       message: 'There was a problem finding the users.'
     }));
+}
+
+UserController.getUser = (req, res) => {
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    return res.status(422).send({ errors: errors.array() })
+  }
+
+  const { userId } = req.params;
+
+  User.findById(userId, '-password')
+    .then(user => {
+      if (!user) {
+        return res.status(404).send({
+          errors: {
+            location: 'params',
+            param: 'userId',
+            value: userId,
+            msg: 'User not found',
+          },
+        })
+      }
+
+      return res.status(200).send({ data: user })
+    })
+    .catch(err => res.status(500).send({
+      errors: err,
+      message: 'There was a problem finding the users.'
+    }))
+}
+
+UserController.updateUser = (req, res) => {
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    return res.status(422).send({ errors: errors.array() })
+  }
+
+  const { userId } = req.params;
+  const toUpdate = req.body;
+
+
+  User.findByIdAndUpdate(userId, toUpdate, { new: true })
+    .then(user => {
+      if (!user) {
+        return res.status(404).send({
+          errors: {
+            location: 'params',
+            param: 'userId',
+            value: userId,
+            msg: 'User not found',
+          },
+        });
+      }
+
+      return res.status(200).send({ data: user });
+    })
+    .catch(err => res.status(500).send({
+      errors: err,
+      message: 'There was a problem finding the users.'
+    }))
 }
 
 module.exports = UserController;

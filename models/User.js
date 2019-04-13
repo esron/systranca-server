f8acd71c27ddbc2285866a1d1e63ae3f765ccc2f
Esron Dtamar <esron.dtamar@gmail.com>
2019-04-13 18:37:16 -0300

    updates user model to use the phoneschema

--- models/User.js
+++ models/User.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose');
+
+const PhoneSchema = new mongoose.Schema({
+  model: String,
+  phoneId: String,
+  status: String,
+});
+
 const UserSchema = new mongoose.Schema({
   name: String,
   email: String,
-  password: String,
+  password: {
+    type: String,
+    select: false,
+  },
   status: String,
+  phones: [PhoneSchema],
 });
+
 mongoose.model('User', UserSchema);
 
 module.exports = mongoose.model('User');

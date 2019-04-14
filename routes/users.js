84e936049f9e02eb8c911652e29b24dda73d2b6f
Esron Dtamar <esron.dtamar@gmail.com>
2019-04-13 21:04:36 -0300

    fix inports before constants

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const router = express.Router();
 const userController = require('../controllers/UserController');
 const phoneController = require('../controllers/PhoneController');
+
+const router = express.Router();
 
 /* Creates a new user. */
 router.post(

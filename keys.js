6373a36bf1b240716f376481cc174ad12fe54702
Esron Dtamar <esron.dtamar@gmail.com>
2019-05-17 21:35:33 -0300

    creates the key generation script

---
+++ keys.js
@@ -0,0 +1,32 @@
+const { generateKeyPair } = require('crypto')
+const fs = require('fs')
+
+const { secret } = require('./config')
+
+generateKeyPair('rsa', {
+  modulusLength: 1024,
+  publicKeyEncoding: {
+    type: 'spki',
+    format: 'pem'
+  },
+  privateKeyEncoding: {
+    type: 'pkcs8',
+    format: 'pem',
+    cipher: 'aes-256-cbc',
+    passphrase: secret
+  }
+}, (err, publicKey, privateKey) => {
+  if (err) throw err
+
+  fs.writeFile('public.key', publicKey, (err) => {
+    if (err) throw err
+
+    console.log('Public key generated successfully')
+  })
+
+  fs.writeFile('private.key', privateKey, (err) => {
+    if (err) throw err
+
+    console.log('Private key generated successfully')
+  })
+})

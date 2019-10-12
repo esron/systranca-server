a2f42ac676148c3f31ceeedd733416971904fadc
franciscoemanuel <francisco.emanuel@hotmail.com.br>
2019-10-12 14:11:07 -0300

    Fix lint errors

--- initDatabase.js
+++ initDatabase.js
@@ -22,19 +22,19 @@ const adminUser = {
   email: 'admin@admin.com',
   status: 'enabled',
   password: 'adminadmin'
-};
+}
 
 User.findOne({ email: adminUser.email })
   .then(user => {
-    if (!user) return User.create(adminUser);
-    else console.log(`Skipping admin user creation, since is already created`);
+    if (!user) return User.create(adminUser)
+    else console.log(`Skipping admin user creation, since is already created`)
   })
   .then(createdUser => {
     if (createdUser) console.log(`User ${createdUser.name} created successfully`)
   })
   .catch(err => {
-    console.log('There was an error creating the Superuser');
-    throw new Error(err);
+    console.log('There was an error creating the Superuser')
+    throw new Error(err)
   }).finally(() => {
-    process.exit(0);
-  });
+    process.exit(0)
+  })

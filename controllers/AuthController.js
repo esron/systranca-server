ca534012082b4ea2c557808aea8a767b9241b024
Vinicius Arré <viniciusarre@gmail.com>
2019-10-27 15:57:31 -0300

    Improves how exports worked for controllers

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,7 +1,5 @@
-const AuthController = {}
-
-AuthController.issueToken = (req, res) => {
-  return res.status(200).send('VEm')
+module.exports = {
+  issueToken (req, res) {
+    return res.status(200).send('VEm')
+  }
 }
-
-module.exports = AuthController
--- controllers/PhoneController.js
+++ controllers/PhoneController.js
@@ -1,77 +1,75 @@
 const { validationResult, checkSchema } = require('express-validator/check')
 const User = require('../models/User')
 
-const PhoneController = {}
-
-PhoneController.validate = (method) => {
-  switch (method) {
-    case 'setUserPhones': {
-      return checkSchema({
-        'phones.*.userId': {
-          in: ['params'],
-          isMongoId: {
-            errorMessage: 'Invalid userId'
-          }
-        },
-        'phones.*.model': {
-          in: ['body'],
-          exists: {
-            errorMessage: 'Model field is required'
+module.exports = {
+  validate (method) {
+    switch (method) {
+      case 'setUserPhones': {
+        return checkSchema({
+          'phones.*.userId': {
+            in: ['params'],
+            isMongoId: {
+              errorMessage: 'Invalid userId'
+            }
           },
-          isEmpty: {
-            errorMessage: 'Model field cannot be empty',
-            negated: true
+          'phones.*.model': {
+            in: ['body'],
+            exists: {
+              errorMessage: 'Model field is required'
+            },
+            isEmpty: {
+              errorMessage: 'Model field cannot be empty',
+              negated: true
+            },
+            trim: true
           },
-          trim: true
-        },
-        'phones.*.phoneId': {
-          in: ['body'],
-          exists: {
-            errorMessage: 'Phone id field is required'
-          },
-          isEmpty: {
-            errorMessage: 'Phone id field cannot be empty',
-            negated: true
-          },
-          trim: true
-        }
-      })
-    }
-  }
-}
-
-PhoneController.setUserPhones = (req, res) => {
-  const errors = validationResult(req)
-
-  if (!errors.isEmpty()) {
-    return res.status(422).send({ errors: errors.array() })
-  }
-
-  const { userId } = req.params
-  const { phones } = req.body
-
-  User.findById(userId)
-    .then(user => {
-      if (!user) {
-        return res.status(404).send({
-          errors: {
-            location: 'params',
-            param: 'userId',
-            value: userId,
-            msg: 'User not found'
+          'phones.*.phoneId': {
+            in: ['body'],
+            exists: {
+              errorMessage: 'Phone id field is required'
+            },
+            isEmpty: {
+              errorMessage: 'Phone id field cannot be empty',
+              negated: true
+            },
+            trim: true
           }
         })
       }
+    }
+  },
 
-      user.phones = phones
-      user.save()
+  setUserPhones (req, res) {
+    const errors = validationResult(req)
 
-      return res.status(200).send({ data: user })
-    })
-    .catch(err => res.status(500).send({
-      errors: err,
-      messafe: 'There was a problem finding the users.'
-    }))
+    if (!errors.isEmpty()) {
+      return res.status(422).send({ errors: errors.array() })
+    }
+
+    const { userId } = req.params
+    const { phones } = req.body
+
+    User.findById(userId)
+      .then(user => {
+        if (!user) {
+          return res.status(404).send({
+            errors: {
+              location: 'params',
+              param: 'userId',
+              value: userId,
+              msg: 'User not found'
+            }
+          })
+        }
+
+        user.phones = phones
+        user.save()
+
+        return res.status(200).send({ data: user })
+      })
+      .catch(err => res.status(500).send({
+        errors: err,
+        messafe: 'There was a problem finding the users.'
+      }))
+  }
 }
-
-module.exports = PhoneController

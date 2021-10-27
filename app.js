a489280629818107b6d1067854f502db22ccb8c0
Esron <esron.dtamar@gmail.com>
2021-10-26 23:27:49 -0300

    move db connection to bin/www

--- app.js
+++ app.js
@@ -2,24 +2,13 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
-const mongoose = require('mongoose')
 const expressValidator = require('express-validator')
-
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 const doorRouter = require('./routes/door')
 const authRouter = require('./routes/auth')
 
 const app = express()
-
-const dbConfig = require('./config/db.js')
-
-const connectUri = dbConfig.getConnectionURI()
-
-mongoose.connect(
-  connectUri,
-  { useNewUrlParser: true, useUnifiedTopology: true }
-)
 
 app.use(logger('dev'))
 app.use(express.json())

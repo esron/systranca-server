73be8522004ea68660ed0128bf08e49859b82712
Esron <esron.dtamar@gmail.com>
2021-10-28 22:47:13 -0300

    Add validateToken middleware to user router

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const userController = require('../controllers/UserController')
 const phoneController = require('../controllers/PhoneController')
+const authController = require('../controllers/AuthController')
 
 const router = express.Router()
 
 /* Creates a new user. */
 router.post(
   '/',
+  authController.validateToken,
   userController.validate('createUser'),
   userController.createUser
 )
@@ -14,12 +16,14 @@ router.post(
 /* GET users listing. */
 router.get(
   '/',
+  authController.validateToken,
   userController.listUsers
 )
 
 /* GET a user. */
 router.get(
   '/:userId',
+  authController.validateToken,
   userController.validate('getUser'),
   userController.getUser
 )
@@ -27,23 +31,27 @@ router.get(
 /* Update a user. */
 router.put(
   '/:userId',
+  authController.validateToken,
   userController.validate('updateUser'),
   userController.updateUser
 )
 
 router.post(
   '/:userId/phones',
+  authController.validateToken,
   phoneController.validate('setUserPhones'),
   phoneController.setUserPhones
 )
 
 router.post(
   '/:userId/pinCode',
+  authController.validateToken,
   userController.createPinCode
 )
 
 router.put(
   '/:userId/pinCode',
+  authController.validateToken,
   userController.updatePinCode
 )
 
